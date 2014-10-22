f79e06cae4c48cda642693aaf8e58d2db84920f2
stisur <stian_s_@hotmail.com>
2014-10-22 17:21:52 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html ng-app='stiansurenApp'>
+	<head>
+		<meta charset="UTF-8">
+		<link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+		<base href="http://folk.ntnu.no/suren/hjemmeside/" target="_self">
+		<meta name="description" content="Stian Sur&eacute;n's Portfolio">
+		<meta name="author" content="Stian Surén">
+		<meta name="keywords" content="html portfolio homepage">
+		<link rel="shortcut icon" href="media/circle.png">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="https://code.angularjs.org/1.3.0-rc.5/angular.min.js"></script>
+		<script src="https://code.angularjs.org/1.3.0-rc.5/angular-route.min.js"></script>
+		<script src="https://code.angularjs.org/1.3.0-rc.5/angular-resource.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+		<script type="text/javascript" src="js/controllers.js"></script>
+		<script type="text/javascript" src="js/services.js"></script>
+		<title>Stian Sur&eacute;n</title>
+	</head>
+	<body>
+		<div ng-view id="mainView"></div>
+	</body>
+</html>
